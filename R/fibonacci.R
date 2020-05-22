6adeb50bb0b676cfca583dd176f4fded631ee3b2
s21810691 <s21810691@stat01.yu.ac.kr>
2020-05-22 14:45:02 +0900

    create fibonacci function

---
+++ R/fibonacci.R
@@ -0,0 +1,17 @@
+#' Compute fibonacci sequence
+#'
+#' This function computes fibonacci sequence.
+#'
+#' @examples
+#'
+#' fibonacciSequence(10)
+#'
+fibonacciSequence <- function(x)
+{
+  a<- c(0,1)
+  for(i in 3:(x-2))
+  {
+    a[i] = a[i-2] + a[i-1]
+  }
+  return(a)
+}
